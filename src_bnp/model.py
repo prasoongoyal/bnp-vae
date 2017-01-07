cb7ea7ceeff0fddf30dfe6b26c69b0f7f5e49628
prasoongoyal <prasoongoyal13@gmail.com>
2017-01-06 21:52:28 -0500

    Completed VI code.

--- src_bnp/model.py
+++ src_bnp/model.py
@@ -7,19 +7,22 @@ import os
 import json
 import numpy as np
 
+from copy import deepcopy
+from scipy.special import digamma
 from util import *
 from layers import *
 from io import open
 from itertools import imap
 
-DEFAULT_ARCH = [np.prod(IMG_DIM), 1024, 1024, NUM_PATHS]
+#DEFAULT_ARCH = [np.prod(IMG_DIM), 1024, 1024, NUM_PATHS]
 DEFAULT_LEARNING_RATE = 1E-3
+LATENT_CODE_SIZE = 10
 
 class Model(object):
   # TODO: Add arguments for architecture, learning rate, etc.
   def __init__(self, batch_size, output_dir):
     self.batch_size = batch_size
-    self.architecture = DEFAULT_ARCH
+    #self.architecture = DEFAULT_ARCH
     self.learning_rate = DEFAULT_LEARNING_RATE
     self.nonlinearity = tf.nn.elu
     self.squashing = tf.nn.sigmoid
@@ -32,8 +35,9 @@ class Model(object):
     handles = self.buildGraph()
     self.session.run(tf.initialize_all_variables())
     
-    (self.x_in, self.ncrp_prior, self.path_prob, self.x_reconstructed, self.rec_cost_mean, 
-     self.kl_cost_mean, self.cost, self.global_step, self.train_op) = handles
+    (self.x_in, self.mu_in, self.log_sigma_in, self.z, self.x_reconstructed, 
+     self.rec_cost_mean, self.kl_cost_mean, self.cost, self.global_step, 
+     self.train_op) = handles
 
   def sampleMultinomial(self, theta_normalized):
     with tf.name_scope('sample_multinomial'):
@@ -51,7 +55,6 @@ class Model(object):
     CONV_FILTER_SIZES = [5, 5, 3]
     CONV_NUM_CHANNELS = [IMG_DIM['channels'], 32, 32, 16]
     POOL_SIZES = [3, 4, 5]
-    LATENT_CODE_SIZE = 10
 
     final_size = (int(IMG_DIM['width'] / np.prod(POOL_SIZES)), 
                   int(IMG_DIM['height'] / np.prod(POOL_SIZES)),
@@ -61,8 +64,10 @@ class Model(object):
     # network inputs
     x_in = tf.placeholder(tf.float32, shape=[None, IMG_DIM['width'], IMG_DIM['height'], 3], 
                           name='x')
-    ncrp_prior = tf.placeholder(tf.float32, shape=[None, self.architecture[-1]], 
-                                name='ncrp_prior')
+    #ncrp_prior = tf.placeholder(tf.float32, shape=[None, NUM_PATHS], 
+    #                            name='ncrp_prior')
+    mu_in = tf.placeholder(tf.float32, shape=[None, LATENT_CODE_SIZE])
+    log_sigma_in = tf.placeholder(tf.float32, shape=[None, 1])
     dropout = tf.placeholder_with_default(1.0, shape=[], name='dropout')
 
     enc_conv_layers = [ConvPool('encoder_conv_pool', conv_kernel_size, conv_output_channels,
@@ -77,7 +82,7 @@ class Model(object):
     x_encoded = composeAll(reversed(enc_fc_layers))(x_flatten)
 
     # probability over paths, to be used in KL-loss
-    path_prob = Dense('path_prob', NUM_PATHS, 1., tf.nn.sigmoid)(x_encoded)
+    # path_prob = Dense('path_prob', NUM_PATHS, 1., tf.nn.sigmoid)(x_encoded)
 
     # mean and standard deviation for sampling latent code
     z_mean= Dense('z_mean', LATENT_CODE_SIZE)(x_encoded)
@@ -101,7 +106,7 @@ class Model(object):
     x_reconstructed = tf.sigmoid(composeAll(dec_conv_layers)(z_reshape))
 
     rec_loss = Model.l2_loss(x_reconstructed, x_in)
-    kl_loss = Model.kl_loss(path_prob, ncrp_prior)
+    kl_loss = Model.kl_loss(z_mean, z_log_sigma, mu_in, log_sigma_in)
 
     with tf.name_scope('rec_cost_mean'):
       rec_cost_mean = tf.reduce_mean(rec_loss)
@@ -121,11 +126,25 @@ class Model(object):
       train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step,
                   name='minimize_cost')
 
-    return (x_in, ncrp_prior, path_prob, x_reconstructed, rec_cost_mean, 
+    return (x_in, mu_in, log_sigma_in, z, x_reconstructed, rec_cost_mean, 
             kl_cost_mean, cost, global_step, train_op)
     
   def train(self, train_data, max_iter=np.inf, max_epochs=np.inf, outdir='./out'):
     saver = tf.train.Saver(tf.all_variables())
+
+    # ncrp hyperparameters
+    self.ALPHA = np.zeros(shape=(NUM_PATHS, LATENT_CODE_SIZE))
+    self.GAMMA = 1.0
+    self.SIGMA_B = 1.0
+    self.SIGMA_Z = 1.0
+
+    # variational parameters
+    var_alpha = np.random.normal(size=(NUM_PATHS, LATENT_CODE_SIZE))
+    var_log_sigma = np.zeros(shape=(NUM_PATHS, LATENT_CODE_SIZE))
+    var_gamma = {}
+    var_phi = {}
+
+    self.latent_codes = {}
 
     try:
       err_train = 0
@@ -138,26 +157,36 @@ class Model(object):
 
       while True:
         x, x_annot, one_epoch_completed = train_data.get_next_batch()
+        #print np.shape(x_annot)
+        mu_true_path = Model.get_true_path_mean(x_annot,
+                                              var_alpha, var_log_sigma, var_gamma, var_phi)
+        batch_size = np.size(x, 0)
+        log_sigma_true_path = np.log(self.SIGMA_Z) * np.ones(shape=(batch_size, 1))
+        #print np.shape(mu_true_path)
+        #print np.shape(log_sigma_true_path)
         feed_dict = {self.x_in: x, 
-                     self.ncrp_prior: Model.compute_ncrp_prior_batch(x_annot, ncrp_prior)}
-        fetches = [self.x_reconstructed, self.path_prob, self.rec_cost_mean,
+                     self.mu_in: mu_true_path,
+                     self.log_sigma_in: log_sigma_true_path}
+        fetches = [self.z, self.x_reconstructed, self.rec_cost_mean,
                   self.kl_cost_mean, self.cost, self.global_step, self.train_op]
 
-        (x_reconstructed, path_prob, rec_cost_mean, kl_cost_mean, cost,
+        (z, x_reconstructed, rec_cost_mean, kl_cost_mean, cost,
          iteration, _) = self.session.run(fetches, feed_dict)
 
-        self.update_path_assignments(path_prob, x_annot)
+        # self.update_path_assignments(path_prob, x_annot)
+        self.update_latent_codes(z, x_annot)
 
         # write model periodically
-        if iteration%1000 == 0:
+        if iteration%1000 == -1:
           saver.save(self.session, os.path.join(self.output_dir, 'model'), 
                      global_step = iteration)
           Model.write_z(self.path_assignments, os.path.join(self.output_dir,
                         'assignments'+unicode(iteration)+'.txt'))
 
-        # recompute ncrp prior periodically
-        if iteration%10 == 0:
-          ncrp_prior = Model.recompute_ncrp(self.path_assignments)
+        # update variational parameters periodically
+        if iteration%1 == 0:
+          var_alpha, var_log_sigma, var_gamma, var_phi = self.update_variational_parameters(
+              var_alpha, var_log_sigma, var_gamma, var_phi)
 
         err_train += cost
         print (('Iter : %d \t ' +
@@ -175,6 +204,111 @@ class Model(object):
                       'assignments'+unicode(iteration)+'.txt'))
         sys.exit(0)
 
+  def update_variational_parameters(self, var_alpha, var_log_sigma, var_gamma, var_phi):
+    print 'Performing variational inference...'
+    var_sigma = np.exp(var_log_sigma)
+    while True:
+      #old_var_alpha, old_var_sigma, old_var_gamma, old_var_phi = var_alpha, \
+      #    var_sigma, var_gamma, var_phi
+      old_var_alpha = deepcopy(var_alpha)
+      old_var_sigma = deepcopy(var_sigma)
+      old_var_gamma = deepcopy(var_gamma)
+      old_var_phi = deepcopy(var_phi)
+      # compute alpha
+      sum_phi_z = NUM_PATHS * [0.0]
+      sum_phi = NUM_PATHS * [0.0]
+      for vidid in self.latent_codes:
+        for frameid in self.latent_codes[vidid]:
+          z_mn = self.latent_codes[vidid][frameid]
+          try:
+            phi_mn = var_phi[vidid][frameid]
+          except KeyError:
+            phi_mn = NUM_PATHS * [1.0 / NUM_PATHS]
+          #print np.shape(z_mn)
+          #print np.shape(phi_mn)
+          for i in range(NUM_PATHS):
+            sum_phi_z[i] += phi_mn[i] * z_mn
+            sum_phi[i] += phi_mn[i]
+      for i in range(NUM_PATHS):
+        var_alpha[i] = (self.ALPHA[i] + sum_phi_z[i]) / (1.0 + sum_phi[i])
+      print 'alpha updated'
+      # compute sigma
+      for i in range(NUM_PATHS):
+        var_sigma[i] = self.SIGMA_B / np.sqrt(1.0 + sum_phi[i])
+      print 'sigma updated'
+      # compute gamma
+      for vidid in self.latent_codes:
+        try:
+          latent_codes_vidid = self.latent_codes[vidid]
+        except KeyError:
+          latent_codes_vidid = {}
+        gamma_m = NUM_EDGES * [[1, self.GAMMA]]
+        for frameid in latent_codes_vidid:
+          phi_mn = NUM_PATHS * [1.0 / NUM_PATHS]
+          for i in range(NUM_PATHS):
+            edges_on_path = Model.get_edges_on_path(i)
+            for j in edges_on_path:
+              #print i, j, np.shape(gamma_m), np.shape(phi_mn), gamma_m[j]
+              gamma_m[j][0] += phi_mn[i]
+            edges_before_path = Model.get_edges_before_path(i)
+            for j in edges_before_path:
+              gamma_m[j][1] += phi_mn[i]
+        var_gamma[vidid] = gamma_m
+      print 'gamma computed'
+      # compute phi
+      for vidid in self.latent_codes:
+        for frameid in self.latent_codes[vidid]:
+          z_mn = self.latent_codes[vidid][frameid]
+          phi_mn = NUM_PATHS * [1.0 / NUM_PATHS]
+          for i in range(NUM_PATHS):
+            phi_mn[i] = -1.0 / (2.0 * self.SIGMA_Z**2) * \
+                        (np.linalg.norm(z_mn - var_alpha[i]) + var_sigma[i]**2)
+            edges_on_path = Model.get_edges_on_path(i)
+            edges_before_path = Model.get_edges_before_path(i)
+            for j in edges_on_path:
+              phi_mn[i] +=  digamma(var_gamma[vidid][j][0]) - \
+                            digamma(var_gamma[vidid][j][0] + var_gamma[vidid][j][1])
+            for j in edges_before_path:
+              phi_mn[i] +=  digamma(var_gamma[vidid][j][1]) - \
+                            digamma(var_gamma[vidid][j][0] + var_gamma[vidid][j][1])
+      print 'phi computed'
+      alpha_diff = np.linalg.norm(var_alpha - old_var_alpha)
+      sigma_diff = np.linalg.norm(var_sigma - old_var_sigma)
+      # print alpha_diff, sigma_diff
+      if (alpha_diff + sigma_diff < 1E-7):
+        break
+    return var_alpha, np.log(var_sigma), var_gamma, var_phi
+
+  @staticmethod
+  def get_edges_on_path(i):
+    result = []
+    i += NUM_INTERNAL_EDGES
+    while (i >= 0):
+      result.append(i)
+      i = int(i/BRANCHING_FACTOR) - 1
+    return result
+
+  @staticmethod
+  def get_edges_before_path(i):
+    result = []
+    i += NUM_INTERNAL_EDGES
+    while (i >= 0):
+      for j in range(int(i/BRANCHING_FACTOR)*BRANCHING_FACTOR, i):
+        result.append(j)
+      i = int(i/BRANCHING_FACTOR) - 1
+    return result
+
+  def update_latent_codes(self, z_batch, x_annot_batch):
+    for (z, x_annot) in zip(z_batch, x_annot_batch):
+      (vidid, frameid) = x_annot
+      #print vidid, frameid
+      try:
+        latent_codes_vid = self.latent_codes[vidid]
+      except KeyError:
+        latent_codes_vid = {}
+      latent_codes_vid[frameid] = z
+      self.latent_codes[vidid] = latent_codes_vid
+    #print self.latent_codes
 
   def sampleGaussian(self, mu, log_sigma):
     # (Differentiably!) draw sample from Gaussian with given shape, 
@@ -195,6 +329,27 @@ class Model(object):
           
       vid_path_assignments[frameid] = int(z[i])
       self.path_assignments[vidid] = vid_path_assignments
+
+  @staticmethod
+  def get_true_path_mean(x_annot_batch, alpha, log_sigma, gamma, phi):
+    true_path_mu_batch = []
+    for x_annot in x_annot_batch:
+      (vidid, frameid) = x_annot
+      # get distribution over paths
+      try:
+        path_prob = phi[vidid][frameid]
+      except KeyError:
+        path_prob = [1.0 / NUM_PATHS] * NUM_PATHS
+      # normalize path prob
+      sum_path_prob = float(sum(path_prob))
+      path_prob = map(lambda x:x/sum_path_prob, path_prob)
+      # sample a path
+      true_path = np.random.choice(NUM_PATHS, 1, path_prob)
+      # true path params
+      true_path_mu = np.random.normal(alpha[true_path], 
+                                      np.diag(np.exp(2 * log_sigma[true_path])))
+      true_path_mu_batch.append(np.squeeze(true_path_mu))
+    return true_path_mu_batch
 
   @staticmethod
   def compute_ncrp_prior_batch(x_annot, ncrp_prior):
@@ -270,15 +425,24 @@ class Model(object):
     fileptr.close()
 
   @staticmethod
-  def kl_loss(theta_normalized, ncrp_prior):
-    offset = 1e-7
+  def kl_loss(mu_pred, log_sigma_pred, mu_in, log_sigma_in):
+    # offset = 1e-7
     with tf.name_scope('kl_loss'):
+      mu_diff = tf.subtract(mu_pred, mu_in, name='mu_diff')
+      mu_diff_norm = tf.reduce_sum(tf.square(mu_diff), axis=1)
+      sigma_norm = tf.reduce_sum(tf.square(tf.exp(log_sigma_pred)))
+      #print type(log_sigma_in)
+      #print log_sigma_in.get_shape()
+      return tf.subtract(1.0/(tf.exp(log_sigma_in) ** 2) * tf.add(mu_diff_norm, sigma_norm),
+                         2.0 * tf.reduce_sum(log_sigma_pred, axis=1))
+      '''
       theta_normalized_ = tf.clip_by_value(theta_normalized, offset, 1-offset)
       ncrp_prior_ = tf.clip_by_value(ncrp_prior, offset, 1-offset)
       log_theta_normalized = tf.log(theta_normalized_, name='log_theta_normalized')
       log_ncrp_prior = tf.log(ncrp_prior_, name='log_ncrp_prior')
       log_diff = tf.subtract(log_theta_normalized, log_ncrp_prior, name='log_diff')
       return tf.reduce_sum(tf.multiply(theta_normalized_, log_diff), 1)
+      '''
     
 
   @staticmethod
--- src_bnp/util.py
+++ src_bnp/util.py
@@ -7,10 +7,14 @@ from functional import partial, compose
 
 batch_size = 2
 IMG_DIM = {'width': 480, 'height': 360, 'channels': 3}
-GAMMA = 1.0   # for ncrp
+#GAMMA = 1.0   # for ncrp
 BRANCHING_FACTOR = 3
 NUM_LEVELS = 4
+NUM_INTERNAL_EDGES = BRANCHING_FACTOR * ((BRANCHING_FACTOR ** (NUM_LEVELS-2) - 1) /
+                                         (BRANCHING_FACTOR - 1))
 NUM_PATHS = BRANCHING_FACTOR ** (NUM_LEVELS - 1)
+NUM_EDGES = BRANCHING_FACTOR * ((BRANCHING_FACTOR ** (NUM_LEVELS-1) - 1) / 
+                                (BRANCHING_FACTOR - 1))
 
 def composeAll(*args):
     """Util for multiple function composition
